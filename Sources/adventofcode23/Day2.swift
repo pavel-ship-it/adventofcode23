ab6e34e92a16b3deaf6927da27e29d3be64d9689
Pavel Yakimenko <yakimenkop@gmail.com>
2023-12-04 16:20:10 +0000

    Day 2, part 1

---
+++ Sources/adventofcode23/Day2.swift
@@ -0,0 +1,80 @@
+
+extension String {
+    func parse() -> (Int, Int, Int) { // (r, g, b)
+        var r = 0, g = 0, b = 0
+        split(separator: ",")
+            .forEach {
+                let str = String($0).replacingOccurrences(of: " ", with: "") // "5red"
+                if str.contains("red") {
+                    r = Int(str.replacingOccurrences(of: "red", with: "")) ?? 0
+                } else if str.contains("green") {
+                    g = Int(str.replacingOccurrences(of: "green", with: "")) ?? 0
+                } else if str.contains("blue") {
+                    b = Int(str.replacingOccurrences(of: "blue", with: "")) ?? 0
+                }
+            }
+        return (r, g, b)
+    }
+
+    func d2p1gameResult() -> Bool {
+        let expectation = (12, 13, 14)
+        // Game 95: 5 blue, 2 red, 9 green; ...
+        guard let q = self.firstIndex(of: ":") else { return false }
+        // " 5 blue, 2 red, 9 green; ..."
+        let result = self[q...].dropFirst()
+        // [" 5 blue, 2 red, 9 green", ...]
+            .split(separator: ";")
+        // [2 9 5, ...]
+            .map { String($0).parse() }
+        // true if ALL expectations are fulfiled
+            .map { $0.0 <= expectation.0 && $0.1 <= expectation.1 && $0.2 <= expectation.2 }
+        // true if all game sets are possible
+            .reduce(into: true) { $0 = $1 && $0 }
+        return result
+    }
+
+    func d2p2gameResult() -> Bool {
+        let expectation = (12, 13, 14)
+        // Game 95: 5 blue, 2 red, 9 green; ...
+        guard let q = self.firstIndex(of: ":") else { return false }
+        // " 5 blue, 2 red, 9 green; ..."
+        let result = self[q...].dropFirst()
+        // [" 5 blue, 2 red, 9 green", ...]
+            .split(separator: ";")
+        // [2 9 5, ...]
+            .map { String($0).parse() }
+        // true if ALL expectations are fulfiled
+            .map { $0.0 <= expectation.0 && $0.1 <= expectation.1 && $0.2 <= expectation.2 }
+        // true if all game sets are possible
+            .reduce(into: true) { $0 = $1 && $0 }
+        return result
+    }
+}
+
+class Task3: Task {
+    func calc(_ inputFile: String) -> Int {
+        let games = fileDataWithEmptyLines(inputFile)
+            .compactMap { $0.d2p1gameResult() }
+        var sum = 0
+        for (i, n) in games.enumerated() {
+            if n {
+                sum += i + 1
+            }
+        }
+        return sum
+    }
+}
+
+class Task4: Task {
+    func calc(_ inputFile: String) -> Int {
+        let games = fileDataWithEmptyLines(inputFile)
+            .compactMap { $0.d2p2gameResult() }
+        var sum = 0
+        for (i, n) in games.enumerated() {
+            if n {
+                sum += i + 1
+            }
+        }
+        return sum
+    }
+}
--- Sources/adventofcode23/main.swift
+++ Sources/adventofcode23/main.swift
@@ -1,2 +1,3 @@
 Task1().run("input1", 55386)
 Task2().run("input1", 54824)
+Task3().run("input2", 2265)
