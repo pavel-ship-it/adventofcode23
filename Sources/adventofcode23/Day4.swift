5e75988d680ce0d716ba66af484ea9076e06d511
Pavel Yakimenko <yakimenkop@gmail.com>
2023-12-06 07:13:35 +0000

    Day 4, part 2

--- Sources/adventofcode23/Day4.swift
+++ Sources/adventofcode23/Day4.swift
@@ -1,18 +1,32 @@
 extension String {
-    func d4p1gameResult() -> Int {
+    func d4p1gameResult() -> Int {                                              // returns number of winning number in card
         guard let q = self.firstIndex(of: ":") else { return 0 }                // "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
         let result = self[q...].dropFirst()                                     // " 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
             .split(separator: "|")                                              // [" 41 48 83 86 17 ", " 83 86  6 31 17  9 48 53"]
-        let winning = result[0].split(separator: " ").compactMap { $0.replacingOccurrences(of: " ", with: "").isEmpty ? nil : Int(String($0).replacingOccurrences(of: " ", with: "")) } // [41, 48, 83, 86, 17]
-        let card = result[1].split(separator: " ").compactMap { $0.replacingOccurrences(of: " ", with: "").isEmpty ? nil : Int(String($0).replacingOccurrences(of: " ", with: "")) }    // [83, 86, 6, 31, 17, 9, 48, 53]
-        let matching = card.compactMap { winning.contains($0) ? $0 : nil }.count // 4
-        return 1 << (matching-1)                                                // 0b100 = 8
+            .map { $0.split(separator: " ").compactMap { Int(String($0).replacingOccurrences(of: " ", with: "")) } } // [[41, 48, 83, 86, 17], [83, 86, 6, 31, 17, 9, 48, 53]]
+        return result[1].compactMap { result[0].contains($0) ? $0 : nil }.count // 4
     }
 }
+
 class Task7: Task {
     func calc(_ inputFile: String) -> Int {
         fileDataWithEmptyLines(inputFile).dropLast()
-            .compactMap { $0.d4p1gameResult() }
+            .compactMap { 1 << ($0.d4p1gameResult()-1) }                        // 0b00000001 << 3 = 0b00000100 = 8
             .reduce(0, +)
+    }
+}
+
+class Task8: Task {
+    func calc(_ inputFile: String) -> Int {
+        let cards = fileDataWithEmptyLines(inputFile).dropLast()
+        var counts = [Int](repeating: 1, count: cards.count)
+        for (i, card) in cards.enumerated() {
+            let count = card.d4p1gameResult()
+            if count == 0 || i+count > cards.count { continue }
+            for j in (i+1...i+count) {                                          // For number of next cards
+                counts[j] += counts[i]                                          // Advance count for number of curret cards
+            }
+        }
+        return counts.reduce(0, +)
     }
 }
